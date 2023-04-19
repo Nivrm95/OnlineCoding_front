386fa1e41323bc295f71b0f25218f0a6512a8c9e
Niv Rahamim <nivrm@gmail.com>
2023-04-19 22:49:02 +0300

    add download and copy code

--- src/componnents/codeBlock/CodeBlock.tsx
+++ src/componnents/codeBlock/CodeBlock.tsx
@@ -1,18 +1,84 @@
-import React, { useRef } from "react";
+// import React, { useRef } from "react";
+// import "./CodeBlock.css";
+// import "@monaco-editor/react";
+// import { Editor } from "@monaco-editor/react";
+
+// const CodeBlock: React.FC = () => {
+//   const editorRef = useRef<any>();
+
+//   function handleEditorDidMount(editor: any, monaco: any) {
+//     editorRef.current = editor;
+//   }
+
+//   return (
+//     <div className="code-block-container">
+//       <div className="code-block-header"></div>
+//       <Editor
+//         height="100%"
+//         width="750px"
+//         defaultLanguage="javascript"
+//         onMount={handleEditorDidMount}
+//         theme="vs-dark"
+//       />
+//     </div>
+//   );
+// };
+
+// export default CodeBlock;
+
+import React, { useRef, useState } from "react";
 import "./CodeBlock.css";
 import "@monaco-editor/react";
 import { Editor } from "@monaco-editor/react";
 
 const CodeBlock: React.FC = () => {
   const editorRef = useRef<any>();
-
+  const codeRef = useRef<HTMLElement>(null);
+  
   function handleEditorDidMount(editor: any, monaco: any) {
     editorRef.current = editor;
   }
 
+  const [downloadButtonText, setDownloadButtonText] =
+    useState("Download Code 📄");
+
+  const handleDownloadClick = () => {
+    if (editorRef.current) {
+      const code = editorRef.current.getValue();
+      const blob = new Blob([code], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "code.txt";
+      a.click();
+      URL.revokeObjectURL(url);
+      setDownloadButtonText("Downloaded! 🡣");
+      setTimeout(() => {
+        setDownloadButtonText("Download Code 📄");
+      }, 2000);
+    }
+  };
+
+  const [copyButtonText, setCopyButtonText] = useState("Copy Code 📷");
+  const handleCopyClick = () => {
+    if (editorRef.current) {
+      const code = editorRef.current.getValue();
+      navigator.clipboard.writeText(code);
+      setCopyButtonText("Copied! ✔");
+      setTimeout(() => {
+        setCopyButtonText("Copy Code 📷");
+      }, 2000);
+    }
+  };
+
   return (
     <div className="code-block-container">
-      <div className="code-block-header"></div>
+      <div className="code-block-header">
+        <button onClick={handleDownloadClick}>
+          {downloadButtonText}
+        </button>
+        <button onClick={handleCopyClick}>{copyButtonText}</button>
+      </div>
       <Editor
         height="100%"
         width="750px"
